39e8d5684e6997463ad6d3c50f8f7d208b18c2d6
吳勖群 <fvgbhn321597@yahoo.com.tw>
2023-06-09 09:52:50 +0800

    預設位置 "木柵動物園"

--- js/leaflet.js
+++ js/leaflet.js
@@ -1,6 +1,8 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([24.9988806, 121.5727678], 15);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 15, //地圖規模
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var marker = L.marker([24.9988806, 121.5727678]).addTo(map); //標記符號
