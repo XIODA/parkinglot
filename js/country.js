9e4f16c9ae04f702548a5c1a27b200effb9f1692
吳勖群 <fvgbhn321597@yahoo.com.tw>
2023-06-09 16:55:06 +0800

    點擊縣市 -> 地圖更換

--- js/country.js
+++ js/country.js
@@ -1,110 +1,113 @@
 window.onload = function () {
-var dropdown = document.getElementById('dropdown');
-var text_title = document.getElementById('text_title');
-var index = dropdown.selectedIndex;
-// console.log(index);
+    var dropdown = document.getElementById('dropdown');
+    var text_title = document.getElementById('text_title');
+    var index = dropdown.selectedIndex;
+    // console.log(index);
 
 
-// var datas = {
-//     "dropdown_country": index,
-// }
-// console.log(datas);
+    // var datas = {
+    //     "dropdown_country": index,
+    // }
+    // console.log(datas);
 
 
 
-$.ajax({
-    type: 'POST',
-    url: './backend/APIs/country/country.php',
-    dataType: 'JSON',
-    success: function (response) {
+    $.ajax({
+        type: 'POST',
+        url: './backend/APIs/country/country.php',
+        dataType: 'JSON',
+        success: function (response) {
 
-        // console.log(response );
-        var text = '<div id="parkinglot_taiwan">全台の停車場</div>'
-        var country_1 = '<option value="-1">請選擇縣市</option>';
-        // console.log(country_1);
-
-        for (var i = 0; i < response.length; i++) {
-            country_1 = country_1 + '<option value="' + response[i].id + '">' + response[i].country + '</option>';
+            // console.log(response );
+            var text = '<div id="parkinglot_taiwan">全台の地圖</div>'
+            var country_1 = '<option value="-1">請選擇縣市</option>';
             // console.log(country_1);
 
-            // console.log(text);
-            // text = text + '<div style="display:none">' + response[i].country + 'の停車場</div>';
-            
-        }
+            for (var i = 0; i < response.length; i++) {
+                country_1 = country_1 + '<option value="' + response[i].id + '">' + response[i].country + '</option>';
+                // console.log(country_1);
 
-        dropdown.innerHTML = country_1;
-       
+                // console.log(text);
+                // text = text + '<div style="display:none">' + response[i].country + 'の停車場</div>';
+
+            }
+
+            dropdown.innerHTML = country_1;
+
             text_title.innerHTML = text;
             // console.log(dropdown);
-        
+
+        }
+
+    })
+
+
+}
+
+function select_country() {
+    
+    // var map = document.getElementById('map'); //標記為木柵動物園
+    console.log(map);
+    var dropdown = document.getElementById('dropdown');
+    var parkinglot_taiwan = document.getElementById('parkinglot_taiwan');
+    // var textContent = parkinglot_taiwan.textContent; //取得
+    var index = dropdown.selectedIndex;
+    // console.log(index_1);
+    var datas = {
+        "dropdown": index,
     }
+    // console.log(parkinglot_taiwan);
 
-})
-
-
-}
-
-function select_country(){
-
-var dropdown = document.getElementById('dropdown');
-var parkinglot_taiwan = document.getElementById('parkinglot_taiwan');
-// var textContent = parkinglot_taiwan.textContent; //取得
-var index = dropdown.selectedIndex;
-// console.log(index_1);
-var datas = {
-    "dropdown": index,
-}
-// console.log(parkinglot_taiwan);
-
-// return;
-$.ajax({
-    type: 'POST',
-    url: './backend/APIs/country/country_1.php',
-    data: datas,
-    dataType: 'JSON',
-    success: function (response_2) {
-        // var country_2 = '<option>請選擇區域</option>';
-        // console.log(response_2);
-        // console.log(response_2);
-        for (var j = 0; j < response_2.length; j++) {
+    // return;
+    $.ajax({
+        type: 'POST',
+        url: './backend/APIs/country/country_1.php',
+        data: datas,
+        dataType: 'JSON',
+        success: function (response_2) {
+            // var country_2 = '<option>請選擇區域</option>';
             // console.log(response_2);
-            parkinglot_taiwan.innerHTML  = response_2[j].country+ 'の停車場';
+            // console.log(response_2);
+            for (var j = 0; j < response_2.length; j++) {
+                // console.log(response_2);
+                parkinglot_taiwan.innerHTML = response_2[j].country + 'の地圖';
+                map.setView([response_2[j].latitude, response_2[j].longitude], 10);
+            }
+
+
+
         }
-        
 
 
+    })
+
+    //行政區
+    var dropdown = document.getElementById('dropdown');
+    // var area_dropdown = document.getElementById('area_dropdown');
+    // console.log(area_dropdown);
+    var index = dropdown.selectedIndex;
+    // var index_1 = area_dropdown.selectedIndex;
+    var datas_1 = {
+        "dropdown": index,
+        // "dropdown_area": index_1,
     }
+    // console.log(datas_1);
+    $.ajax({
+        type: 'POST',
+        url: './backend/APIs/country/area.php',
+        data: datas_1,
+        dataType: 'JSON',
+        success: function (response_1) {
+            var country_2 = '<option>請選擇區域</option>';
+            for (var j = 0; j < response_1.length; j++) {
+                country_2 = country_2 + '<option value="' + response_1[j].parkinglot_id + '">' + response_1[j].country_area + '</option>';
+            }
+            area_dropdown.innerHTML = country_2;
 
-
-})
-
-//行政區
-var dropdown = document.getElementById('dropdown');
-// var area_dropdown = document.getElementById('area_dropdown');
-// console.log(area_dropdown);
-var index = dropdown.selectedIndex;
-// var index_1 = area_dropdown.selectedIndex;
-var datas_1 = {
-    "dropdown": index,
-    // "dropdown_area": index_1,
-}
-// console.log(datas_1);
-$.ajax({
-    type: 'POST',
-    url: './backend/APIs/country/area.php',
-    data: datas_1,
-    dataType: 'JSON',
-    success: function (response_1) {
-        var country_2 = '<option>請選擇區域</option>';
-        for (var j = 0; j < response_1.length; j++) {
-            country_2 = country_2 + '<option value="' + response_1[j].parkinglot_id + '">' + response_1[j].country_area + '</option>';
+            // console.log(area_dropdown);
         }
-        area_dropdown.innerHTML = country_2;
-
-        console.log(area_dropdown);
-    }
 
 
-})
+    })
 
 }
--- js/leaflet.js
+++ js/leaflet.js
@@ -1,4 +1,10 @@
-var map = L.map('map').setView([24.9988806, 121.5727678], 15); //標記為木柵動物園
+var map = L.map('map'); //標記為木柵動物園
+map.setView([24.9988806, 121.5727678], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18, //地圖規模
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
 
 //2d
 function twoD() {
@@ -38,20 +44,4 @@ var circle = L.circle([24.9988806, 121.5727678], {
 
 marker.bindPopup("<b>這裡是木柵</b><br>動物園.").openPopup();
 circle.bindPopup("木柵附近商圈");
-polygon.bindPopup("I am a polygon.");
 
-
-
-var baselayers = {
-    'OpenStreetMap.Mapnik': L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'),
-    'OpenStreetMap.DE': L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'),
-    'OpenStreetMap.CH': L.tileLayer('https://tile.osm.ch/switzerland/{z}/{x}/{y}.png'),
-    'OpenStreetMap.France': L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png'),
-    'OpenStreetMap.HOT': L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'),
-    'OpenStreetMap.BZH': L.tileLayer('https://tile.openstreetmap.bzh/br/{z}/{x}/{y}.png'),
-    'OpenTopoMap': L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png')
-};
-
-var overlays = {};
-L.control.layers(baselayers, overlays).addTo(map);
-baselayers['OpenStreetMap.Mapnik'].addTo(map);
